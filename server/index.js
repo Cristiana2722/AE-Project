c64f565171c08f5d7fcf10f27349470ea1716e70
Cristiana <cristiana071201@gmail.com>
2025-11-13 14:24:16 +0200

    Added project base from seminar

---
+++ server/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cors = require('cors');
+
+const { User } = require('./database/models');
+
+// routes
+const userRoutes = require('./routes/user.routes');
+const authRoutes = require('./routes/auth.routes');
+const productRoutes = require('./routes/product.routes');
+
+const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 7778; //fallback if doesn't find key
+
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'Hello' });
+})
+// routes
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/products', productRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server successfully started on port ${PORT}!`);
+})
