d4a626b4537bd732da671a3f4a9331fd7a31c23b
Cristiana <cristiana071201@gmail.com>
2025-11-14 07:46:34 +0200

    Styled the site and added products

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -130,7 +130,7 @@ export default function ProductsPage() {
           {isAdmin && (
             <button
               onClick={handleCreateClick}
-              className="inline-flex items-center rounded-md bg-indigo-600 px-4 py-2 text-sm font-semibold text-white hover:bg-indigo-500"
+              className="inline-flex items-center rounded-md px-4 py-2 text-sm font-semibold text-white bg-rose-400 hover:bg-rose-600"
             >
               <svg className="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 4v16m8-8H4" />
@@ -189,7 +189,7 @@ export default function ProductsPage() {
               </div>
                 <button
                   onClick={() => handleAddToCart(product.product_id)}
-                  className="relative z-20 w-full mt-2 inline-flex items-center justify-center rounded-md bg-indigo-600 px-3 py-1 text-sm font-semibold text-white hover:bg-indigo-500"
+                  className="relative z-20 w-full mt-2 inline-flex items-center justify-center rounded-md px-3 py-1 text-sm font-semibold text-white bg-rose-400 hover:bg-rose-600"
                 >
                   Add to Cart
                 </button>
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -37,18 +37,13 @@ export default function Register() {
   return (
       <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
-            className="mx-auto h-10 w-auto"
-          />
-          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">Create an account</h2>
+          <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-black">Create an account</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form onSubmit={handleSubmit} className="space-y-6">
             <div>
-              <label htmlFor="name" className="block text-sm/6 font-medium text-gray-100">
+              <label htmlFor="name" className="block text-sm/6 font-medium text-black">
                 Name
               </label>
               <div className="mt-2">
@@ -60,12 +55,12 @@ export default function Register() {
                   onChange={handleChange}
                   required
                   autoComplete="name"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
             </div>
             <div>
-              <label htmlFor="email" className="block text-sm/6 font-medium text-gray-100">
+              <label htmlFor="email" className="block text-sm/6 font-medium text-black">
                 Email address
               </label>
               <div className="mt-2">
@@ -77,14 +72,14 @@ export default function Register() {
                   onChange={handleChange}
                   required
                   autoComplete="email"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
             </div>
 
             <div>
               <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm/6 font-medium text-gray-100">
+                <label htmlFor="password" className="block text-sm/6 font-medium text-black">
                   Password
                 </label>
               </div>
@@ -97,7 +92,7 @@ export default function Register() {
                   onChange={handleChange}
                   required
                   autoComplete="current-password"
-                  className="block w-full rounded-md bg-white/5 px-3 py-1.5 text-base text-white outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
+                  className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-black outline-1 -outline-offset-1 outline-white/10 placeholder:text-gray-500 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-500 sm:text-sm/6"
                 />
               </div>
             </div>
@@ -105,16 +100,16 @@ export default function Register() {
             <div>
               <button
                 type="submit"
-                className="flex w-full justify-center rounded-md bg-indigo-500 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-indigo-400 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
+                className="flex w-full justify-center rounded-md bg-rose-400 px-3 py-1.5 text-sm/6 font-semibold text-white hover:bg-rose-600 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-rose-500"
               >
                 Register
               </button>
             </div>
           </form>
 
-          <p className="mt-10 text-center text-sm/6 text-gray-400">
+          <p className="mt-10 text-center text-sm/6 text-gray-600">
             Already have an account?{' '}
-            <Link to="/login" className="font-semibold text-indigo-400 hover:text-indigo-300">
+            <Link to="/login" className="font-semibold text-rose-600 hover:text-rose-400">
               Sign in
             </Link>
           </p>
